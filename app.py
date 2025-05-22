27b70afbf2e08536726cbcd9212b18eabc08f605
VishalGirase <70579998+VishalGirase@users.noreply.github.com>
2025-05-21 21:59:50 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Portfolio", layout="centered")
+
+st.title("👋 Hi, I'm [Vishal Girase]")
+st.subheader("Versatile Data Professional | Expert Data Engineer & Analyst | Proficient in ETL, SQL, Python | Data Modeling & Visualization Enthusiast")
+
+st.write("Welcome to my portfolio site")
+
+st.markdown("""
+- 📧 Email: vishal1997girase@gmail.com  
+- 💼 [LinkedIn](https://www.linkedin.com/in/vishal-girase/)  
+- 🐙 [GitHub](https://github.com/VishalGirase)  
+""")
+
+st.image("vishal_girase.png", width=250)
